820f56c1361658cd1734c85a362f4bb2ae158995
rubyenk <zhb976153@icloud.com>
2017-03-21 16:31:12 +0800

    = =

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,3 +1,6 @@
 class Sport < ApplicationRecord
   validates :title,:winner,:loser,:wp,:lp, presence: true
+  validates :winner,:loser,:wp,:lp, numericality: { only_integer: true }
+  validates :winner,:loser, length:{maximum:2}
+  validates :wp,:lp, length:{maximum:3}
 end
