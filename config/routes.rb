db10989e3cf15d5264de9045e390eef04e3a9571
1625081 <sanjin@buaa.edu.cn>
2017-03-25 01:37:08 +0800

    add-user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/signup'
+
   resources :sports do
     collection{post :search}
   end
   root 'sports#index'
+
+  get "signup" => "users#signup", :as => "signup"
+  get "login" => "users#login", :as => "login"
+  resources :users, only: [:create]
+  post "create_login_session" => "users#create_login_session"
+  delete "logout" => "users#logout", :as => "logout"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323054627) do
+ActiveRecord::Schema.define(version: 20170324155705) do
 
   create_table "sports", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20170323054627) do
     t.string   "status"
     t.string   "hour"
     t.string   "minute"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
